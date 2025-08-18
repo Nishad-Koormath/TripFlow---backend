3fa24da29d087ba0e1627d396b417d608911a9a9
nishad <nishadkoormath@gmail.com>
2025-08-18 11:11:56 +0530

    add booking viewset with owner and admin permissions

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Bookings
+from .serializers import BookingSerializer
+from .permissions import IsOwnerOrAdmin
+
 
 # Create your views here.
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            return Bookings.objects.all()
+        return Bookings.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        package = serializer.validated_data['package']
+        num_people = serializer.validated_data['num_people']
+        total_price = package.price * num_people
+        serializer.save(user=self.request.user, total_price=total_price)
