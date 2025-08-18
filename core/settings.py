d6880353b4b43ec255b404418832d40462feb6cf
nishad <nishadkoormath@gmail.com>
2025-08-18 12:39:27 +0530

    add pagination support to api responses

--- core/settings.py
+++ core/settings.py
@@ -142,3 +142,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
