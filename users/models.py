be7a9d6f5daf0ec53bb6a765bb87b28a1d95b003
nishad <nishadkoormath@gmail.com>
2025-08-25 16:44:23 +0530

    Updated User model: removed role, using is_staff & is_superuser for access control

--- users/models.py
+++ users/models.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    class Roles(models.TextChoices):
-        VISITOR = 'visitor', 'Visitor'
-        CUSTOMER = 'customer', 'Customer'
-        STAFF = 'staff', 'Staff'
-        ADMIN = 'admin', 'Admin'
-        
-    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.CUSTOMER)
     phone = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
--- users/serializers.py
+++ users/serializers.py
@@ -1,33 +1,56 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
-        )
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     password = serializers.CharField(write_only=True)
-    
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'phone', 'role']
-    
+        fields = ['id', 'username', 'email', 'password',
+                  'first_name', 'last_name', 'phone', 'role']
+
     def create(self, validated_data):
         user = User.objects.create_user(
-            username = validated_data['username'],
-            email = validated_data['email'],
-            password = validated_data['password'],
-            first_name = validated_data.get('first_name', ''),
-            last_name = validated_data.get('last_name', ''),
-            phone = validated_data.get('phone', ''),
-            role = validated_data.get('role', User.Roles.CUSTOMER),
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            phone=validated_data.get('phone', ''),
+            role=validated_data.get('role', User.Roles.CUSTOMER),
         )
         return user
-    
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "phone", "role"]
-    
+        fields = ["id", "username", "email",
+                  "first_name", "last_name", "phone", "role"]
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+        token['is_staff'] = user.is_staff
+        token['is_superuser'] = user.is_superuser
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['is_staff'] = self.user.is_staff
+        data['is_superuser'] = self.user.is_superuser
+        return data
